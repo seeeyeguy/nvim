6ebc0c28b297663faadafe489ddbd7c941d2d00a
Nate Charbonneau <nathaniel.charbonneau@gmail.com>
2026-06-04 21:19:29 -0400

    fixed treesitter config

--- after/queries/zig/textobjects.scm
+++ after/queries/zig/textobjects.scm
@@ -16,21 +16,18 @@
 
 ; ─── Conditionals ────────────────────────────────────────────────────────────
 (if_statement) @conditional.outer
-(if_statement
-  consequence: (block) @conditional.inner)
+(if_statement (block) @conditional.inner)
 
 ; ─── Loops ───────────────────────────────────────────────────────────────────
 (for_statement) @loop.outer
-(for_statement
-  body: (block) @loop.inner)
+(for_statement (block) @loop.inner)
 
 (while_statement) @loop.outer
-(while_statement
-  body: (block) @loop.inner)
+(while_statement (block) @loop.inner)
 
 ; ─── Blocks ──────────────────────────────────────────────────────────────────
 (block) @block.outer
 (block (_) @block.inner)
 
 ; ─── Comments ────────────────────────────────────────────────────────────────
-(line_comment) @comment.outer
+(comment) @comment.outer
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,65 +1,59 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    event = { "BufReadPre", "BufNewFile" },
+    lazy = false,
     dependencies = {
-	"windwp/nvim-ts-autotag",
-	"nvim-treesitter/nvim-treesitter-textobjects",
+        "windwp/nvim-ts-autotag",
+        "nvim-treesitter/nvim-treesitter-textobjects",
     },
     build = ":TSUpdate",
     config = function()
-	require("nvim-treesitter.configs").setup({
-	    auto_install   = false,
-	    sync_install   = false,
-	    ignore_install = {},
-	    modules        = {},
-	    highlight  = { enable = true },
-	    indent     = { enable = true },
-	    autotag    = { enable = true },
-	    ensure_installed = {
-		"bash", "c", "css", "dockerfile", "graphql",
-		"html", "htmldjango", "json", "markdown", "markdown_inline",
-		"nu", "python", "query", "sql", "vim", "vimdoc", "yaml", "zig",
-	    },
-	    ignore_install = { "lua" },
-	    incremental_selection = { enable = true, keymaps = {} },
-	    textobjects = {
-		move = {
-		    enable = true, set_jumps = true,
-		    goto_next_start     = {
-			["]b"] = { query = "@code_cell.inner" },
-			["<M-PageDown>"] = { query = "@code_cell.inner" },
-			["]f"] = { query = "@function.outer", desc = "TS: Next function" },
-			["]l"] = { query = "@loop.outer",     desc = "TS: Next loop" },
-		    },
-		    goto_previous_start = {
-			["[b"] = { query = "@code_cell.inner" },
-			["<M-PageUp>"] = { query = "@code_cell.inner" },
-			["[f"] = { query = "@function.outer", desc = "TS: Prev function" },
-			["[l"] = { query = "@loop.outer",     desc = "TS: Prev loop" },
-		    },
-		},
-		select = {
-		    enable = true, lookahead = true,
-		    keymaps = {
-			["ib"] = { query = "@code_cell.inner", desc = "TS: in code block" },
-			["ab"] = { query = "@code_cell.outer", desc = "TS: around code block" },
-			["af"] = { query = "@function.outer" },
-			["if"] = { query = "@function.inner" },
-			["ac"] = { query = "@class.outer" },
-			["ic"] = { query = "@class.inner" },
-			["aa"] = { query = "@parameter.outer" },   -- 'a' for argument
-			["ia"] = { query = "@parameter.inner" },
-			["ai"] = { query = "@conditional.outer" },
-			["ii"] = { query = "@conditional.inner" },
-			["al"] = { query = "@loop.outer" },
-			["il"] = { query = "@loop.inner" },
-			["ak"] = { query = "@block.outer" },
-			["ik"] = { query = "@block.inner" },
-			["am"] = { query = "@comment.outer" },
-			["im"] = { query = "@comment.outer" },
-		    },
-		},
-	    },
-	})
+        local select = require('nvim-treesitter-textobjects.select')
+        local move   = require('nvim-treesitter-textobjects.move')
+        local config = require('nvim-treesitter-textobjects.config')
+
+        config.update({
+            select = {
+                enable = true,
+                lookahead = true,
+            },
+            move = {
+                enable = true,
+                set_jumps = true,
+            },
+        })
+
+        -- ── Select keymaps ────────────────────────────────────────────────
+        local keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["ai"] = "@conditional.outer",
+            ["ii"] = "@conditional.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+            ["ak"] = "@block.outer",
+            ["ik"] = "@block.inner",
+            ["am"] = "@comment.outer",
+            ["im"] = "@comment.outer",
+            ["ab"] = "@code_cell.outer",
+            ["ib"] = "@code_cell.inner",
+        }
+
+        for key, query in pairs(keymaps) do
+            vim.keymap.set({ "x", "o" }, key, function()
+                select.select_textobject(query, "textobjects")
+            end, { desc = "TS: " .. query })
+        end
+
+        -- ── Move keymaps ──────────────────────────────────────────────────
+        vim.keymap.set("n", "]f", function() move.goto_next_start("@function.outer",  "textobjects") end, { desc = "TS: Next function" })
+        vim.keymap.set("n", "[f", function() move.goto_previous_start("@function.outer", "textobjects") end, { desc = "TS: Prev function" })
+        vim.keymap.set("n", "]l", function() move.goto_next_start("@loop.outer",      "textobjects") end, { desc = "TS: Next loop" })
+        vim.keymap.set("n", "[l", function() move.goto_previous_start("@loop.outer",  "textobjects") end, { desc = "TS: Prev loop" })
+        vim.keymap.set("n", "]b", function() move.goto_next_start("@code_cell.inner", "textobjects") end, { desc = "TS: Next code cell" })
+        vim.keymap.set("n", "[b", function() move.goto_previous_start("@code_cell.inner", "textobjects") end, { desc = "TS: Prev code cell" })
     end,
 }
